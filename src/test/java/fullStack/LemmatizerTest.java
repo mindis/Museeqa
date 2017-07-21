fe9f5e1f6ebdc164b545d2f70d28682ebf0a1a42
Sherzod Hakimov <sherzod@mercury.ai>
2017-07-21 15:23:32 +0200

    lemmatizer test

--- src/test/java/fullStack/LemmatizerTest.java
+++ src/test/java/fullStack/LemmatizerTest.java
@@ -27,12 +27,12 @@ public class LemmatizerTest {
         
         
         Set<String> lemmas2 = Lemmatizer.lemmatize("people", CandidateRetriever.Language.EN);
-        
+        System.out.println(lemmas2);
         Assert.assertEquals(true, lemmas2.contains("person"));
         
         
-        Set<String> lemmas3 = Lemmatizer.lemmatize("hecho", CandidateRetriever.Language.ES);
-        
-        Assert.assertEquals(true, lemmas3.contains("hacer"));
+        Set<String> lemmas3 = Lemmatizer.lemmatize("tocaba", CandidateRetriever.Language.ES);
+        System.out.println(lemmas3);
+        Assert.assertEquals(true, lemmas3.contains("tocar"));
     }
 }
